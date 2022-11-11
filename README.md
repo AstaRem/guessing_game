1a4caa7dac34357700c0bd7322b4f2ab6e66e675
Asta Remeikiene <asta.rem@hotmail.com>
2022-11-11 12:13:47 -0800

    updated README.md file

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # guessing_game
 
-test1
+- User is asked to choose game difficulty level.
+- User is prompted to guess a number.
+- User gets feedback and can enter another number, if quess is not correct this time.
+- After user gets the correct number, game tells how many guesses it took to get the number right.
--- guess.py
+++ guess.py
@@ -1,6 +1,5 @@
 #!/home/resume/test_dir2_VM2/venv/bin/python3
 
-
 import random
 
 minimum = 0
@@ -13,10 +12,6 @@ print("a - Easy: 0-10")
 print("b - Medium: 0-50")
 print("c - Hard: 0-100")
 print("d - Hardcore: 0-1000")
-
-
-
-
 
 while maximum == None:
 	level_user= input("Please enter a letter a, b, c or d -  which level you want to play: ")
@@ -33,12 +28,7 @@ while maximum == None:
 		maximum == None
 		level_user= input("Invalid entry. Please press enter.")
 
-
-
 answer = random.randint(minimum, maximum)
-
-print(answer, level, maximum)
-
 
 while answer != guess and maximum != 0:
 	guess = int(input(f"Guess the number from {minimum} to {maximum}: "))
